4e929b8e75596e9226057dcb97c033975200461f
SlackRainDrop489 <quinn967056@my.yosemite.edu>
2025-09-11 10:49:45 -0700

    test

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# Objects_Classes_Lab
+# BankAccount Class
 
+A C++ class for making and editing a bank account.
+
+## Data Dictionary
+**hi**
---
+++ main.cpp
@@ -0,0 +1,180 @@
+/*
+Quinn Alvine
+9/11/25
+This program is for making and editing a bank account
+Lab Activities: Objects and Classes I
+*/
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <limits>
+
+using namespace std;
+
+class BankAccount { // This is the BankAccount class
+    private:
+        string accountNumber;
+        string accountHolderName;
+        double balance;
+    public:
+        BankAccount();
+        BankAccount(string acountNum, string HolderName, double bal);
+        string getAccountNumber();
+        string getAccountHolderName();
+        double getBalance();
+        void setAccountHolderName(string accountHoldName);
+        double deposit(double amount);
+        double withdraw(double amount);
+};
+
+string BankAccount::getAccountNumber() { // This will return the account number
+    return accountNumber;
+}
+
+string BankAccount::getAccountHolderName() { // This will return the bank account holder name
+    return accountHolderName;
+}
+
+double BankAccount::getBalance() { // This will get the bank account balance
+    return balance;
+}
+
+void BankAccount::setAccountHolderName(string accountHoldName) { // This will set the bank account holder name
+    accountHolderName = accountHoldName;
+}
+
+double BankAccount::deposit(double amount) { // This will deposit money into the bank account
+    balance += amount;
+    return balance;
+}
+
+double BankAccount::withdraw(double amount) { // This will withdraw money from the bank account
+    balance -= amount;
+    return balance;
+}
+
+BankAccount::BankAccount() { // This is the default constructor
+    accountNumber = "";
+    accountHolderName = "";
+    balance = 0.0;
+}
+
+BankAccount::BankAccount(string acountNum, string HolderName, double bal) { // This is the parameterized constructor
+    accountNumber = acountNum;
+    accountHolderName = HolderName;
+    balance = bal;
+}
+
+void Menu(int accountNum) { // This will display the menu
+    cout << "---------------------------" << endl;
+    cout << "You are in account number: " << accountNum << endl;
+    cout << "1. Set account holder name" << endl;
+    cout << "2. Get account holder name" << endl;
+    cout << "3. Get account number" << endl;
+    cout << "4. Get balance" << endl;
+    cout << "5. Change balance" << endl;
+    cout << "6. Create new account" << endl;
+    cout << "7. Change account" << endl;
+    cout << "8. Quit" << endl;
+    cout << "---------------------------" << endl;
+}
+
+int main() { // Main function
+    BankAccount BankAccount1; // This creates the first account
+    vector<BankAccount> BankAccounts; // This creates a vector of bank accounts
+    BankAccounts.push_back(BankAccount1); // This adds the first account to the vector
+    int currentAccountNum = 0; // This is the current account
+    int currentDisplayAccountNum = 1; // This is the account number to be displayed
+    int input; // This is the input
+    int accountInputNum; // This is the input for changing accounts
+    string newAccountName; // This is for holding the new name of an account
+    double newBalance; // This is for holding the new balance of an account
+    do { // This is the main loop
+        Menu(currentDisplayAccountNum); // This will display the menu
+        cout << "Enter your choice: "; // This will prompt you for your input
+        if (!(cin >> input)) { // This will make sure that you are inputting an int
+            cout << "Invalid input" << endl;
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        } else {
+            cout << endl;
+            switch (input) { // This is the main switch
+                case 1: // Change account name
+                    cout << "Enter account holder name: ";
+                    cin.ignore();
+                    getline(cin, newAccountName);
+                    BankAccounts[currentAccountNum].setAccountHolderName(newAccountName);
+                    break;
+                case 2: // Get account holder name
+                    cout << "The name of this account is: " << BankAccounts[currentAccountNum].getAccountHolderName() << endl;
+                    break;
+                case 3: // Get account number
+                    cout << "The account number is: " << BankAccounts[currentAccountNum].getAccountNumber() << endl;
+                    break;
+                case 4: // Get balance
+                    cout << "The balance is: " << BankAccounts[currentAccountNum].getBalance() << endl;
+                    break;
+                case 5: {
+                    // Change balance
+                    cout << "Whould you like to make a (1) Deposit or (2) a Withdraw: ";
+                    int optionInput;
+                    cin >> optionInput;
+                    switch (optionInput) {
+                        case 1: // Make a deposit
+                            cout << endl << "How much would you like to deposit: ";
+                            double deposit;
+                            cin >> deposit;
+                            BankAccounts[currentAccountNum].deposit(deposit);
+                            break;
+                        case 2: // Make a withdraw
+                            cout << endl << "You currently have $" << BankAccounts[currentAccountNum].getBalance() << endl;
+                            cout << "How much would you like to withdraw: ";
+                            double withdraw;
+                            cin >> withdraw;
+                            if (withdraw > BankAccounts[currentAccountNum].getBalance()) {
+                                cout << endl << "There is not enough money in your account." << endl;
+                            } else {
+                                BankAccounts[currentAccountNum].withdraw(withdraw);
+                            }
+                            break;
+                    }
+                    break;
+                }
+                case 6: {
+                    // Create new account
+                    cout << "Please enter an account name: ";
+                    cin.ignore();
+                    getline(cin, newAccountName);
+                    cout << endl << "Please enter an account balance: ";
+                    cin >> newBalance;
+                    BankAccount newAccount(to_string(BankAccounts.size() + 1), newAccountName, newBalance);
+                    BankAccounts.push_back(newAccount);
+                    break;
+                }
+                case 7: // Change account
+                    if (BankAccounts.size() == 1) {
+                        cout << "There is only one bank account" << endl;
+                    } else {
+                        cout << "There are " << BankAccounts.size() << " bank accounts" << endl;
+                        cout << "Please enter account number: ";
+                        cin >> accountInputNum;
+                        if (accountInputNum < 0 or accountInputNum > BankAccounts.size()) {
+                            cout << endl << "There is no account with this number" << endl;
+                        } else {
+                            currentAccountNum = accountInputNum - 1;
+                            currentDisplayAccountNum = accountInputNum;
+                        }
+                    }
+                    break;
+                case 8: // Quit
+                    cout << "Quit";
+                    break;
+                default:
+                    cout << "Please enter a valid choice." << endl;
+                    break;
+            }
+        }
+    } while (input != 8); // will run while input is not 8
+    return 0;
+}
